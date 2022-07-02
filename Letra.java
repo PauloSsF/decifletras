fde7b91f6594996eb756cafa3424c6fc65fe4d1e
Paulo Sérgio de Souza Filho <54003071+PauloSsF@users.noreply.github.com>
2022-07-02 00:08:42 +0000

    Initial commit

---
+++ Letra.java
@@ -0,0 +1,14 @@
+package letra;
+public class Letra {
+    private String text;
+    private int letters;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String newText) {
+        this.text = newText;
+    }
+
+}
